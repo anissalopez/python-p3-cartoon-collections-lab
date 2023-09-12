5ef46ae2f49eec2d38469b6c31abee5420fef33f
anissalopez <anissa965@gmail.com>
2023-09-12 10:05:12 -0400

    python practice

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,29 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
 
-def summon_captain_planet():
-    pass
+    for i in range(len(dwarves)):
+        print(f"{i+1}. {dwarves[i]}")
+    
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def summon_captain_planet(cartoons):
+    new_list = [f"{cartoon.title()}!" for cartoon in cartoons]
+    return new_list
+
+def long_planeteer_calls(calls):
+    call_length = False
+    for call in calls:
+        if len(call) > 4:
+            call_length = True
+    return call_length
+    
+
+
+
+def find_the_cheese(cheese_array):
+    cheese_varieties = ["cheddar", "gouda", "camembert"]
+    for cheese in cheese_varieties:
+        if cheese in cheese_array:
+            return cheese
+        
+
+
